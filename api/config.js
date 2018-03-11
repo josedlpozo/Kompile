325d4ae990f439f5ebdbfd1b7ad832642d776108
Jose del Pozo <josedlpozo@gmail.com>
2018-03-11 13:23:18 +0100

    Remove postgresql from production

--- api/config.js
+++ api/config.js
@@ -5,22 +5,10 @@ module.exports = {
   },
   test: {
     dialect: 'sqlite',
-    storage: './db.test.sqlite'
+    storage: ':memory:'
   },
   production: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOSTNAME,
-    dialect: 'postgres',
-    pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000
-    },
-
-    // http://docs.sequelizejs.com/manual/tutorial/querying.html#operators
-    operatorsAliases: false
+    dialect: 'sqlite',
+    storage: './db.production.sqlite'
   }
 };
