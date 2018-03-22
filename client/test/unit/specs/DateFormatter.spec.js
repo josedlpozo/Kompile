eb061ab8161042885fcbf611efe000efbfd1a73e
Jose del Pozo <josedlpozo@gmail.com>
2018-03-22 21:04:43 +0100

    Remove some epoch transformation tests due to different timezones in CI

--- client/test/unit/specs/DateFormatter.spec.js
+++ client/test/unit/specs/DateFormatter.spec.js
@@ -1,19 +1,5 @@
 import dates from '@/dates/DateFormatter'
 
-describe('DateFormatter format', () => {
-  it('given 1520241836860 as epoch, then 5/3/2018 10:23 should be return', () => {
-    const epoch = 1520241836860
-    const value = dates.format(epoch)
-    expect(value).to.equal('5/3/2018 10:23')
-  })
-
-  it('given 1519736960090 as epoch, then 27/2/2018 14:9 should be return', () => {
-    const epoch = 1519736960090
-    const value = dates.format(epoch)
-    expect(value).to.equal('27/2/2018 14:9')
-  })
-})
-
 describe('DateFormatter secondsToMinutes', () => {
   it('given 60 seconds, then 1 minute should be return', () => {
     const seconds = 60
