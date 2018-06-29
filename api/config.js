5bcdb224b9aa39ab679c9527fac25a4a90163988
Jose del Pozo <jmdelpozo@idealista.com>
2018-06-29 09:01:44 +0200

    Add grafana to project
    - Remove web VUE client
    - Add grafana provisioning to add postgres datasource
    - Add new postgresql image to save data instead of sqlite

--- api/config.js
+++ api/config.js
@@ -8,7 +8,16 @@ module.exports = {
     storage: ':memory:'
   },
   production: {
-    dialect: 'sqlite',
-    storage: './db.production.sqlite'
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOSTNAME,
+    dialect: 'postgres',
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    }
   }
 };
